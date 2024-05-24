e0beb8c7d967fc5a4a64259a17caa11295c40227
Daniel Szabo <d.g.szabo-2017@hull.ac.uk>
2024-05-24 10:01:13 +0100

    Add event listeners for button click to append new element to DOM, mouseover, and mouseout events

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const myButton = document.querySelector("button");
 console.log(myButton);
 
 //my JS is expecting a user action on the page
+//in my addEvenetListener method, i have 2 parameters:
+//1. the event i am listening for
+//2. the anonymus function i want to run when the event happens --> we only use it once, so we don't need to name it
 myButton.addEventListener("click", function () {
   console.log("I was clicked");
 });
@@ -18,4 +21,33 @@ myButton.addEventListener("mouseover", function () {
 
 myButton.addEventListener("mouseout", function () {
   console.log("I was moused out");
+});
+
+//examples
+//a button to count from 0 to infinity and beyond!
+const countingButton = document.querySelector("#counting-button");
+
+//we declare the variable outside the function so it doesn't reset every time the function runs
+let count = 0;
+
+countingButton.addEventListener("click", function () {
+  count++;
+  console.log(count);
+  countingButton.textContent = count;
+});
+
+//when we click the button, a DOM elemetn appers on the page
+const mainContainer = document.querySelector(".main-container");
+const messageButton = document.getElementById("message-button");
+console.log(mainContainer);
+console.log(messageButton);
+
+messageButton.addEventListener("click", function () {
+  //create element to contain the message
+  let message = document.createElement("p");
+  //add content to the p tag
+  message.textContent = "Hello World!";
+  //append the p tag to the div parent
+  //parent.appendchild(child)
+  mainContainer.appendChild(message);
 });
